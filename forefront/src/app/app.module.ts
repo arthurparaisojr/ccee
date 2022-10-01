ea2a85c3c993f44222b380180f9617ed7fa2bb9a
Arthur Paraiso Jr <arthurparaisojr@gmail.com>
2022-10-01 12:41:31 -0300

    Ajustes

--- forefront/src/app/app.module.ts
+++ forefront/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeituraComponent } from './leitura/leitura.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeituraComponent
   ],
   imports: [
     BrowserModule,
